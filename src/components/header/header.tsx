c8ae8d6ad56a402d32b0b2fb869051d86bb65479
Christi Kazakov <ckazakov18@gmail.com>
2025-01-16 15:39:07 -0700

    wip: initial project setup for user info personal site

---
+++ src/components/header/header.tsx
@@ -0,0 +1,20 @@
+import { AppBar, Container, Toolbar } from "@mui/material";
+import React from "react";
+
+const Header = () => {
+  
+
+  return (
+    <AppBar position='static'>
+      <Container maxWidth='xl'>
+        <Toolbar>
+          Header Toolbar
+        </Toolbar>
+      </Container>
+      
+    </AppBar>
+      
+  );
+};
+
+export default Header;
