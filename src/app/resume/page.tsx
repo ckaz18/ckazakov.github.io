658c37651de54e82e945b4469357cd0ef8f3758e
Christi Kazakov <ckazakov18@gmail.com>
2025-01-18 12:08:00 -0700

    add default theme keys; update resume to have grid with Contact section and blank experience section for now

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,9 +1,60 @@
+'use client';
+
+import { Container, Paper, Typography } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import Grid2 from "@mui/material/Grid2";
+import Link from "next/link";
+import EmailIcon from '@mui/icons-material/Email';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+  padding: theme.spacing(3),
+  height: '100%',
+}));
+
+const ContactItem = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  marginBottom: theme.spacing(2),
+}));
+
 
 const Resume = () => {
   return (
-    <section>
-      Sup! This is my resume page
-    </section>
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Grid2 container spacing={2}>
+         <Grid2 size={4} sx={{ xs: 12, md: 4,}}>
+         <Item elevation={3}>
+           <Typography variant='h4' gutterBottom>Contact Info</Typography>
+           <ContactItem>
+             <EmailIcon sx={{ mr: 2 }} />
+             <Link href="mailto:ckazakov18@gmail.com">ckazakov18@gmail.com</Link>
+           </ContactItem>
+           <ContactItem>
+             <LocalPhoneIcon sx={{ mr: 2 }} />
+             1+(303)-503-5168
+           </ContactItem>
+           <ContactItem>
+             <LinkedInIcon sx={{ mr: 2 }} />
+             <Link href="https://www.linkedin.com/in/ckazakov/" target="_blank">LinkedIn Profile</Link>
+           </ContactItem>
+           <ContactItem>
+             <GitHubIcon sx={{ mr: 2 }} />
+             <Link href="https://github.com/ckaz18" target="_blank">GitHub Profile</Link>
+           </ContactItem>
+         </Item>
+       </Grid2>
+        <Grid2 size={8}>
+        <Item>
+          {/* Right column content */}
+
+          </Item>
+        </Grid2>
+      </Grid2>
+    </Container>
   );
 }
 
--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,4 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme, ThemeOptions } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
 const theme = createTheme({
@@ -18,10 +18,10 @@ const theme = createTheme({
     },
     grey: {
       50: '#fafafa',
-      100:'#f5f5f5',
-      200:'#eeeeee',
-      300:'#e0e0e0',
-      400:'#bdbdbd',
+      100: '#f5f5f5',
+      200: '#eeeeee',
+      300: '#e0e0e0',
+      400: '#bdbdbd',
       500: '#9e9e9e',
       600: '#757575',
       700: '#616161',
@@ -36,6 +36,54 @@ const theme = createTheme({
       default: '#efefef',
     },
   },
-});
+  spacing: (factor: number) => `${0.25 * factor}rem`,
+  typography: {
+    fontFamily: [
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif'
+    ].join(','),
+    h1: {
+      fontSize: '2.5rem',
+      fontWeight: 500,
+    },
+    h2: {
+      fontSize: '2rem',
+      fontWeight: 500,
+    },
+    h3: {
+      fontSize: '1.75rem',
+      fontWeight: 500,
+    },
+    h4: {
+      fontSize: '1.5rem',
+      fontWeight: 500,
+    },
+    h5: {
+      fontSize: '1.25rem',
+      fontWeight: 500,
+    },
+    h6: {
+      fontSize: '1rem',
+      fontWeight: 300,
+    },
+    body1: {
+      fontSize: '1rem',
+      lineHeight: 1.5,
+    },
+    body2: {
+      fontSize: '0.875rem',
+      lineHeight: 1.43,
+    },
+  },
+  components: {
+    MuiContainer: {
+
+    }
+  }
+} as ThemeOptions);
+
+// = 0.25 * 2rem = 0.5rem = 8px
 
 export default theme;
