d89bde5182f0152a3dabd7df0002f70f7129f02f
Christi Kazakov <ckazakov18@gmail.com>
2025-02-01 18:41:05 -0700

    switch to use new server instead of github

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/ckazakov.github.io',
-  assetPrefix: '/ckazakov.github.io/',
+  basePath: '',
   images: {
     unoptimized: true,
   }
